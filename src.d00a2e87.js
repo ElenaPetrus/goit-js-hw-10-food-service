698942e89c0f4f90e4f945463a263cd4f37885aa
ElenaPetrus <leliksmp@ukr.net>
2021-07-25 18:08:37 +0300

    Updates

--- src.d00a2e87.js
+++ src.d00a2e87.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),console.log("Hello! This is me");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-10-food-service/src.18754f2e.js.map
+//# sourceMappingURL=/goit-js-hw-10-food-service/src.d00a2e87.js.map
