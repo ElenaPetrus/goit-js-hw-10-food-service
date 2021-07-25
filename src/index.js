17bd6a56bb55f2a41d8fc5dcef890a4cea4bcd92
ElenaPetrus <leliksmp@ukr.net>
2021-07-25 21:16:41 +0300

    hbs done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import './sass/main.scss'
+import menuListTpl from './templates/menu-items.hbs'
+import menuData from './menu.json';
+// console.table(menuData);
 
+const refs = {
+    menuList:document.querySelector('.js-menu'),
+}
 
-console.log("Hello! This is me");
+const  MenuListMarkup  = createMenuListMarkup (menuData);
+refs.menuList.insertAdjacentHTML("beforeend", MenuListMarkup);
+
+function createMenuListMarkup (menuData){
+    return  menuData.map (menuItem => menuListTpl(menuItem)).join ('');
+  
+  }
+  
